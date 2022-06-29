6d4908e1c933475fd77fe7187c58223734f4bdb8
Trew Boisvert <trewboisvert@gmail.com>
2022-06-29 11:12:10 -0700

    Add table of contents json route

--- server.py
+++ server.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from mockData import kats_chapter, trews_chapter
 
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    """Simple greeting."""
+    """Simple greeting. Book Cover"""
 
     return render_template("base.html")
+
+@app.route("/table-of-contents.json")
+def toc():
+    """Returns chapters"""
+
+    return jsonify([kats_chapter, trews_chapter])
 
 
 if __name__ == "__main__":
