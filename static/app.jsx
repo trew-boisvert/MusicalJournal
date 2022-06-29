0c876c3a4761a99dd03ec563c66977d6200ba003
Trew Boisvert <trewboisvert@gmail.com>
2022-06-29 11:45:58 -0700

    Pseudocode table of contents component

--- static/app.jsx
+++ static/app.jsx
@@ -4,6 +4,15 @@ function BasicComponent(props) {
     return (<div><p>Roots Baby!</p></div>)
 }
 
+/*
+Table of contents
+set up state to hold data
+use useeffect to make fetch request
+make an array with data we received (some kinda loop)
+render that array
+
+*/
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<BasicComponent/>)
