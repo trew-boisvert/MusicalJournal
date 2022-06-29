eb61bf7af9961d838412d2d03105c7dc6939ae8f
Trew Boisvert <trewboisvert@gmail.com>
2022-06-29 11:00:48 -0700

    Created mock data

---
+++ mockData.py
@@ -0,0 +1,49 @@
+"""
+chapter: {
+    title:
+    image: 
+    playlist: link to spotifiy/apple music playlist id
+    entries : [
+        {title: 
+        body:
+        images:}
+        ]
+    }
+"""
+
+
+kat_chapter = { "title": "Spring Clean the New Home",
+                "image": "https://www.gardendesign.com/pictures/images/675x529Max/site_3/helianthus-yellow-flower-pixabay_11863.jpg"
+                "playlist": "id string goes here", 
+                "entries": [
+                  {
+                      "title": "Frittata day",
+                      "body": "I made a frittata today. I used tomatoes, basil, and onions and had it with a side of salad. It was my first time cooking with cast iron!",
+                      "images": ["https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fill,g_center,w_730,h_913/k%2Farchive%2F073225a69897a6022f3a9ce30b43f01e107a0858", "https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/266425_2200-1200x628.jpg"],
+                      "date": "date goes here"
+                  },
+                  {
+                      "title": "Cleaning the Living Room",
+                      "body": "I survived the cleaning",
+                      "images": [],
+                      "date": "date goes here"
+                  }
+                ]
+              }
+
+trews_chapter = {"title": "Pride",
+                "image": "https://i.guim.co.uk/img/media/d853c0def58d649385d8ca9d8bd8e06b1438b3ad/0_78_4441_2665/master/4441.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=96b9df8279e24a7484b00f2b45eb7dd6",
+                "playlist": "link to something gay", 
+                "entries": [
+                    {
+                        "title": "Trans March",
+                        "body": "Root bought me a cute hat and gave me a unicorn patch to sew onto my bag.",
+                        "images": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYTHz_m9fOQaA0EKYl7R5D9uEPvtNsJlLDFUAEVIwAvnDBExBJOQ_4mrJCiyrTwiZvxJU&usqp=CAU",
+                        "date": "date time thingy"
+                    },     {
+                        "title": "Sunday Pride",
+                        "body": "So many people, so drunk, so rowdy, I just want breakfast.",
+                        "images": "https://static01.nyt.com/images/2019/06/08/world/08warsaw-pride2/08warsaw-pride2-videoSixteenByNineJumbo1600.jpg",
+                        "date": "another date"
+                    }
+                ]}
