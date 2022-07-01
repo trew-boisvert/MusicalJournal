febf9c811106f2e3d35941474c5d5a36cd4f6a2d
Trew Boisvert <trewboisvert@gmail.com>
2022-07-01 10:49:15 -0700

    Create basic table of contents and content components
    Co-authored-by: Katrina Huber-Juma <katrina.huber@gmail.com>

--- static/app.jsx
+++ static/app.jsx
@@ -1,7 +1,32 @@
 // alert("woot")
+function Content({content}){
+
+    const entriesList = content.entries.map((entry)=>{return <li>{entry.title}</li>})
+
+
+    return <li key={content.title}>{content.title}<ul>{entriesList}</ul></li>
+}
+
+
+function TableOfContents(){
+    const [ contents, setContents] = React.useState([]);
+
+    React.useEffect(()=>{
+        fetch('/table-of-contents.json')
+            .then(response=>response.json())
+            .then(result => setContents(result))
+    }, [])
+
+    const listItems = [];
+
+    for (const content of contents){
+        listItems.push(<Content content={content}/>)
+    }
+    return <ul>{listItems}</ul>
+}
 
 function BasicComponent(props) {
-    return (<div><p>Roots Baby!</p></div>)
+    return (<div><TableOfContents/><p>Roots Baybay!</p></div>)
 }
 
 /*
